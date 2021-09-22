80687c1950a770c5a62c1acc8a03712cfc577b4b
william koller <wkoller25@gmail.com>
2021-09-22 01:15:03 -0300

    feat: should be throw if continent name already exists

--- src/modules/continents/services/add-continent/add-continent.service.ts
+++ src/modules/continents/services/add-continent/add-continent.service.ts
@@ -1,5 +1,5 @@
 import { ContinentEntity } from '@/infra/typeorm/entities/continent-entity/continent.entity';
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { AddContinentDto } from '../../dtos/add-continent/add-continent.dto';
 import { ContinentsRepository } from '../../repositories/continents.repository';
 
@@ -10,6 +10,15 @@ export class AddContinentService {
   public async addContinent(
     addContinentDto: AddContinentDto,
   ): Promise<ContinentEntity> {
+    const continentExists = await this.continentsRepo.loadByName(
+      addContinentDto.name,
+    );
+
+    if (continentExists) {
+      throw new ConflictException(
+        'There is already a continent with that name.',
+      );
+    }
     return await this.continentsRepo.addContinent(addContinentDto);
   }
 }

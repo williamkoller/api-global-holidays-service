e8e1a042ab4a753a396c4f3cedd9707e5eb8f24f
william koller <wkoller25@gmail.com>
2021-09-21 23:23:26 -0300

    feat: add AddContinentService in ContinentsModule

--- src/modules/continents/continents.module.ts
+++ src/modules/continents/continents.module.ts
@@ -1,8 +1,10 @@
 import { ContinentEntity } from '@/infra/typeorm/entities/continent-entity/continent.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AddContinentService } from './services/add-continent/add-continent.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ContinentEntity])],
+  providers: [AddContinentService],
 })
 export class ContinentsModule {}

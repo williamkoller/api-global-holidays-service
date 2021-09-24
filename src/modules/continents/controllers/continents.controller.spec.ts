33d28f2a00a13eb3a70ec974a4fac4e88b935291
william koller <wkoller25@gmail.com>
2021-09-23 21:46:37 -0300

    test: should be throw when service throw

--- src/modules/continents/controllers/continents.controller.spec.ts
+++ src/modules/continents/controllers/continents.controller.spec.ts
@@ -1,18 +1,43 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { AddContinentService } from '../services/add-continent/add-continent.service';
 import { ContinentsController } from './continents.controller';
 
 describe('ContinentsController', () => {
   let controller: ContinentsController;
+  let service: AddContinentService;
 
   beforeEach(async () => {
+    const mockService = {
+      addContinent: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: AddContinentService,
+          useFactory: () => mockService,
+        },
+      ],
       controllers: [ContinentsController],
     }).compile();
 
     controller = module.get<ContinentsController>(ContinentsController);
+    service = module.get<AddContinentService>(AddContinentService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('addContinent()', () => {
+    it('should be throw when service throw', async () => {
+      (service.addContinent as jest.Mock).mockRejectedValue(
+        new BadRequestException(),
+      );
+      await expect(controller.addContinent).rejects.toThrow(
+        new BadRequestException(),
+      );
+    });
   });
 });

16b1a71a5fbcd93246847e464ba471b918897318
william koller <wkoller25@gmail.com>
2021-10-11 23:03:40 -0300

    test: add updateContinent(), should be called findOne with correct params

--- src/modules/continents/repositories/continents.repository.spec.ts
+++ src/modules/continents/repositories/continents.repository.spec.ts
@@ -30,6 +30,7 @@ describe('ContinentRepository', () => {
     continentsRepository.create = jest.fn();
     continentsRepository.findOne = jest.fn();
     continentsRepository.find = jest.fn();
+    continentsRepository.merge = jest.fn();
     continentsRepository.createQueryBuilder = jest.fn().mockReturnValue({
       where: jest.fn().mockReturnThis(),
       getOne: jest.fn().mockResolvedValue('África'),
@@ -40,41 +41,59 @@ describe('ContinentRepository', () => {
     expect(continentsRepository).toBeDefined();
   });
 
-  it('should be called save with correct params', async () => {
-    continentsRepository.save = jest.fn().mockReturnValue(mockContinent());
-    continentsRepository.create = jest.fn().mockReturnValue(mockContinent());
-    await continentsRepository.addContinent(mockContinent());
-    expect(continentsRepository.save).toBeCalledWith(mockContinent());
+  describe('addContinent()', () => {
+    it('should be called save with correct params', async () => {
+      continentsRepository.save = jest.fn().mockReturnValue(mockContinent());
+      continentsRepository.create = jest.fn().mockReturnValue(mockContinent());
+      await continentsRepository.addContinent(mockContinent());
+      expect(continentsRepository.save).toBeCalledWith(mockContinent());
+    });
+
+    it('should be returns created data', async () => {
+      (continentsRepository.save as jest.Mock).mockReturnValue(mockContinent());
+      expect(await continentsRepository.addContinent(mockContinent())).toEqual(
+        mockContinent(),
+      );
+    });
   });
 
-  it('should be returns created data', async () => {
-    (continentsRepository.save as jest.Mock).mockReturnValue(mockContinent());
-    expect(await continentsRepository.addContinent(mockContinent())).toEqual(
-      mockContinent(),
-    );
+  describe('loadByName()', () => {
+    it('should be called loadByName with correct params', async () => {
+      const result = await continentsRepository.loadByName('África');
+      expect(result).toEqual('África');
+    });
   });
 
-  it('should be called loadByName with correct params', async () => {
-    const result = await continentsRepository.loadByName('África');
-    expect(result).toEqual('África');
+  describe('createQueryBuilder()', () => {
+    it('should be called continentsRepository with createQueryBuilder must be true', async () => {
+      (continentsRepository.createQueryBuilder as jest.Mock).mockReturnValue(
+        'continents',
+      );
+      expect(continentsRepository.createQueryBuilder).toBeTruthy();
+    });
   });
 
-  it('should be called continentsRepository with createQueryBuilder must be true', async () => {
-    (continentsRepository.createQueryBuilder as jest.Mock).mockReturnValue(
-      'continents',
-    );
-    expect(continentsRepository.createQueryBuilder).toBeTruthy();
+  describe('loadById()', () => {
+    it('should be called loadById with correct params', async () => {
+      continentsRepository.findOne = jest.fn().mockReturnValue(1);
+      await continentsRepository.loadById(1);
+      expect(continentsRepository.findOne).toBeCalledWith(mockQuery);
+    });
   });
 
-  it('should be called loadById with correct params', async () => {
-    continentsRepository.findOne = jest.fn().mockReturnValue(1);
-    await continentsRepository.loadById(1);
-    expect(continentsRepository.findOne).toBeCalledWith(mockQuery);
+  describe('loadAll()', () => {
+    it('should be called loadAll with correct params', async () => {
+      continentsRepository.find = jest.fn().mockReturnValue(mockContinents());
+      await continentsRepository.loadAll();
+      expect(await continentsRepository.find()).toEqual(mockContinents());
+    });
   });
 
-  it('should be called loadAll with correct params', async () => {
-    continentsRepository.find = jest.fn().mockReturnValue(mockContinents());
-    await continentsRepository.loadAll();
-    expect(await continentsRepository.find()).toEqual(mockContinents());
+  describe('updateContinent()', () => {
+    it('should be called findOne with correct params', async () => {
+      continentsRepository.findOne = jest.fn().mockReturnValue(mockContinent);
+      await continentsRepository.updateContinent(1);
+      expect(continentsRepository.findOne).toBeCalledWith(1);
+    });
   });
 });
